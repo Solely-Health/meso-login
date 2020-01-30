c6191425775a9ba695a5f9e451b41c64070d2863
Mikkal <mcnultymikkal@gmail.com>
2020-01-30 14:18:36 -0800

    add init and close methods on database type

--- database/database.go
+++ database/database.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type Database struct {
+	DB *sql.DB
+}
+
+func (database *Database) InitializeDB(DbUser, DbPassword, DbPort, DbHost, DbName string) {
+	var err error
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+
+	// database.DB, err = gorm.Open(Dbdriver, DBURL)
+	database.DB, err = sql.Open("postgres", DBURL)
+	if err != nil {
+		fmt.Printf("Cannot connect to database")
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the database")
+	}
+}
+
+func (database *Database) CloseDB() {
+	database.DB.Close()
+}
